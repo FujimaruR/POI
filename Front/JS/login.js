7850e6cdc69aa6e1c25ec5b774940ec861f421b7
LuisEsparza-bg <luis.esparzalz@uanl.edu.mx>
2023-09-14 13:13:27 -0600

    Cambios Login(HTML,CSS,JS)
    Cambios pa estilo chido

--- Front/JS/login.js
+++ Front/JS/login.js
@@ -4,9 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     paymentForm.addEventListener("submit", function (event) {
         event.preventDefault();
         alert("Iniciando sesión.....")
-        // Aquí puedes establecer la URL de la página a la que deseas redirigir al usuario.
         const nuevaPagina = "/Front/mensajes.html";
-        // Redirecciona a la nueva página.
         window.location.href = nuevaPagina;
     });
 });
